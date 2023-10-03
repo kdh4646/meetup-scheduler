84f9e34dcf443ab07842c23d0d860a5d8f84f451
Derek Kweon <kdh4646@hanmail.net>
2023-10-03 00:14:05 -0700

    2023-10-03 fixed dependencies

---
+++ pages/news/[newsId].js
@@ -0,0 +1,6 @@
+//file name with '[]' allows Next.js to load pages dynamically
+function DetailPage() {
+  return <h1>The Detail Page</h1>;
+}
+
+export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,7 @@
-//In Next.js folders also considered as a part of path
+/*
+  In Next.js folders also considered as a part of path 
+  index.js file indicates the start of that path segment
+*/
 function NewsPage() {
   return <h1>The News Page</h1>;
 }
