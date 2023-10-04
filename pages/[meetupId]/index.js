f75645fdefe8dd59829f6610c9e63a1e5e0ff488
Derek Kweon <kdh4646@hanmail.net>
2023-10-03 17:28:57 -0700

    2023-10-03 added detail page with css

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://test.com"
+      title="11"
+      address="22"
+      description="33"
+    />
+  );
+}
+
+export default MeetupDetails;
