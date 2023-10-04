f5774132f63675f5fa8a256c56eab8c45f4deb20
Derek Kweon <kdh4646@hanmail.net>
2023-10-03 17:09:23 -0700

    2023-10-03 Setting common layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Layout from "../components/layout/Layout";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //setting common layout for all pages
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
